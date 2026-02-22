36cc417b1bc10ec0da0e2cc6b4a8b6a1e602ab0c
peterbergerdev <peterberger@MacBookPro.home>
2026-02-22 01:16:48 +0100

    minimal functional student layout with data testids for later tests

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'students' },
+  {
+    path: 'students',
+    loadComponent: () => import('./students/students.page').then((m) => m.StudentsPage),
+  },
+];
